2ea4bfbfc0af06434548fce361324f4f2af8d86e
Tyler Lawson <tyler@secondspectrum.com>
2021-09-11 12:01:33 -0400

    first commit - setup elder js

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const { getRollupConfig } = require('@elderjs/elderjs');
+const svelteConfig = require('./svelte.config');
+module.exports = [
+  ...getRollupConfig({
+    svelteConfig,
+    replacements: {
+      preventAssignment: true,
+    },
+  }),
+];
